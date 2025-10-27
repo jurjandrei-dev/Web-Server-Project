cf9e151b19cf021cb7ffb29c8234e2089f4f5859
jurjandrei-dev <jurj.andrei.dev@gmail.com>
2025-10-27 11:35:54 +0200

    Client Socket + APICLient

---
+++ Client/ClientSocket.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include<SFML/Network.hpp>
+class ClientSocket{
+private:
+    sf::TcpSocket socket;
+public:
+    bool connect(sf::IpAddress ip, unsigned short port);
+    std::string receive();
+    bool send(const std::string& message);
+};
